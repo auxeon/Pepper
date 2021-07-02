99ae142424e11c24063f96943d4ac5f3bcbe37ed
Abhikalp Unakal <abhikalpunakal@outlook.com>
2021-07-01 18:34:06 -0700

    cleanup

--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc -lglfw -framework CoreVideo -framework OpenGL -framework IOKit -framework Cocoa -framework Carbon main.c ../core/ps_chronon.c ../lib/glad/src/glad.c -I ../lib/glad/include -I ../lib/glad/src
